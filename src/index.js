06c24aef0b2f2549043c1e6b4a06cf6f156562a2
Dennis Wakahia <dwakahia@cytonn.com>
2022-01-12 19:18:33 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./style.scss";
+//import "./style.css";
+console.log("Hello webpack!");
+console.log("hello world")
+
+import "./style.scss";
+console.log("Hello webpack!");
+
+const fancyFunc = () => {
+    return [1, 2];
+};
+
+const [a, b] = fancyFunc();
